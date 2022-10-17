972530a02429aaf693d67463f4e9d72abf7c241d
danydifilippo <danydifilippo@gmail.com>
2022-10-17 16:34:50 +0200

    initial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,58 @@
-// async function showApi() {
-//     let musiclink = await fetch ('https://striveschool-api.herokuapp.com/api/deezer/search?q=maneskin');
-//     let responseText = await musiclink.json();
-//     console.log(responseText);
-//     let music = responseText.data;
-//     console.log(music);
+
+    async function showApi(n) {
+        let musiclink = await fetch (n);
+        let responseText = await musiclink.json();
+        console.log(responseText);
+        let music = responseText.data;
+        console.log(music);
+    }
+    const searchBar = document.getElementById('searchBar');
+    console.log(searchBar);
+    const urlBase = 'q=radiohead'
+    function searchArtist() {
+        let params = new URLSearchParams(urlBase);
+        let query = params.get('q');
+        let input = searchBar.value
+        console.log(query);
+        params.set('q',input);
+        let data = params.toString();
+        let newUrl = `https://striveschool-api.herokuapp.com/api/deezer/search?${data}`
+        console.log(newUrl); 
+        showApi(newUrl) ; 
+    }
+
+    let arrayArtists = ['Night Skinny','Radiohead','Lazza','Lizzo','Maneskin','Noyz Narcos','Salmo','Gemitaiz','Twenty one Pilots','Madonna']
+
+    for (let i=0; i<arrayArtists.length; i++) {
+        let newUrl2 = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${arrayArtists[i]}`;
+        showApi(newUrl2);
+        
+    }
+
     
-//     for (let i=0; i<music.length; i++){
-//         let showcard = document.getElementById('cards')
-//         let scheda = document.createElement('div');
-//         scheda.classList.add('card', 'bg-transparent', 'border-0')
-//         scheda.innerHTML += `<img src="${music[i].album.cover_medium}" class="card-img-top rounded-circle" alt="cover album Maneskin">
-//         <div class="card-body"><h5 class="card-title bg-white bg-opacity-25 rounded-pill ">${music[i].title}</h5>
-//         <audio controls style="width: 180px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
-//         <a href="${music[i].artist.link}" class="btn btn-primary">Go to link</a>`
-//         showcard.appendChild(scheda);
-//     }
+
+        
+
+    for (let i=0; i<music.length; i++){
+        let showcard = document.getElementById('cards')
+        let scheda = document.createElement('div');
+        scheda.classList.add('card', 'bg-transparent', 'border-0')
+        scheda.innerHTML += `<img src="${music[i].album.cover_medium}" class="card-img-top rounded-circle" alt="cover album Maneskin">
+        <div class="card-body"><h5 class="card-title bg-white bg-opacity-25 rounded-pill ">${music[i].title}</h5>
+        <audio controls style="width: 180px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
+        <a href="${music[i].artist.link}" class="btn btn-primary">Go to link</a>`
+        showcard.appendChild(scheda);
+    }
     
  
-//     const searchBar = document.getElementById('searchBar');
+    // const searchBar = document.getElementById('searchBar');
  
-//     console.log(searchBar);
-//     searchBar.addEventListener('keyup',(e) =>{
-//         const searchString = e.target.value;
-//         const filteredSongs = music.filter( song =>{
-//         return song.title.toLowerCase().includes(searchString.toLowerCase())
-//     });
+    // console.log(searchBar);
+    // searchBar.addEventListener('keyup',(e) =>{
+    //     const searchString = e.target.value;
+    //     const filteredAlbum = music.filter( song =>{
+    //     return song.title.toLowerCase().includes(searchString.toLowerCase())
+    // });
 //     console.log(filteredSongs)
 //     let showcard = document.getElementById('cards')
 //     showcard.innerHTML=''
