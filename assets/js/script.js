251ffcf9187f6e6a5976bb51a77fb292f2dcf7a4
EarthDragon10 <lagudadev@gmail.com>
2022-10-18 13:31:58 +0200

    fix player audio js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,127 @@
+async function showApi(n) {
+	let musiclink = await fetch(n);
+	let responseText = await musiclink.json();
+	let music = responseText.data;
+	console.log(music);
 
-    async function showApi(n) {
-        let musiclink = await fetch (n);
-        let responseText = await musiclink.json();
-        let music = responseText.data;
-        console.log(music);
-
-
-        function card() {
-            let showcard = document.getElementById('artists')
-            showcard.innerHTML += `<div class="card border-0">
+	function card() {
+		let showcard = document.getElementById("artists");
+		showcard.innerHTML += `<div class="card border-0">
             <img src="${music[0].artist.picture_medium}" class="card-img-top rounded-circle" alt="artista">
-            <button id="${music[2].preview} onclick="playCard(this.id), pauseCard()"></button>
+             <audio id='player-album'>
+            <source src="${music[2].preview}" type='audio/mpeg'>
+            </audio>
+            <button class='btn_audio-preview' onclick="togglePlay()"><i id="playIcon" class="bi bi-play-circle-fill d-block"></i></button>
             <div class="card-body">
             <h5 class="card-title text-white">${music[0].artist.name}</h5>
             <p>Artists</p>
-            </div>`
-        };
-        card()
+            </div>`;
+	}
+	card();
 
-        function albums() {
-            let showAlbum = document.getElementById('albums')
-            showAlbum.innerHTML += `<div class="card border-0 bg-dark">
+	function albums() {
+		let showAlbum = document.getElementById("albums");
+		showAlbum.innerHTML += `<div class="card border-0 bg-dark">
             <img src="${music[2].album.cover_medium}" class="card-img-top" alt="album">
-            <button id="${music[2].preview}" onclick="togglePlay(this.id)"><i id="playIcon" class="bi bi-play-circle-fill d-block"></i></button>
+            <audio id='player-album'>
+            <source src="${music[2].preview}" type='audio/mpeg'>
+            </audio>
+            <button class='btn_audio-preview' onclick="togglePlay()"><i id="playIcon" class="bi bi-play-circle-fill d-block"></i></button>
             <div class="card-body">
             <h5 class="card-title text-white">${music[2].album.title}</h5>
             <p>${music[2].artist.name}</p>
-            </div>`
-        };
-        albums()
-        }
-        
-        let play = false;
-        
-        function togglePlay(audio){
-        let sound = new Audio(audio)
-        play ? pauseCard() : playCard();
-         function playCard(){
-            sound.play();
-            play=true;
-        }
-        function pauseCard(){
-            sound.pause();
-            play=false;
-        }
-    }
-    
+            </div>`;
+	}
+	albums();
+}
 
-        
-        // for (let i=0; i<music.length; i++){
-        //     let showcard = document.getElementById('cards')
-        //     showcard.innerHTML += `<div class="card bg-transparent border-0">
-        //     <img src="${music[i].album.cover_medium}" class="card-img-top rounded-circle" alt="cover album Maneskin">
-        //     <div class="card-body">
-        //     <h5 class="card-title bg-white bg-opacity-25 rounded-pill ">${music[i].title}</h5>
-        //     <audio controls style="width: 180px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
-        //     <a href="${music[i].artist.link}" class="btn btn-primary">Go to link</a></div>`
-        // }
-    
+let play = false;
 
+function togglePlay() {
+	let audioElement = document.getElementById("player-album");
 
-    const searchBar = document.getElementById('searchBar');
-    console.log(searchBar);
-    const urlBase = 'q=radiohead'
-    function searchArtist() {
-        let params = new URLSearchParams(urlBase);
-        let query = params.get('q');
-        let input = searchBar.value
-        console.log(query);
-        params.set('q',input);
-        let data = params.toString();
-        let newUrl = `https://striveschool-api.herokuapp.com/api/deezer/search?${data}`
-        console.log(newUrl); 
-        showApi(newUrl) ; 
-    }
+	console.log(play);
 
-    let arrayArtists = ['Night Skinny','Radiohead','Lazza','Lizzo','Maneskin','Noyz Narcos','Salmo','Gemitaiz','Twenty one Pilots','Madonna']
+	play ? pauseCard() : playCard();
 
+	function playCard() {
+		audioElement.play();
 
-    for (let i=0; i<arrayArtists.length; i++) {
-        let newUrl2 = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${arrayArtists[i]}`;
-        showApi(newUrl2)
-    }
+		play = true;
 
-    
-    // for (let i=0; i<music.length; i++){
-    //     let showcard = document.getElementById('cards')
-    //     showcard.innerHTML += `<div class="card bg-transparent border-0">
-    //     <img src="${music[i].album.cover_medium}" class="card-img-top rounded-circle" alt="cover album Maneskin">
-    //     <div class="card-body">
-    //     <h5 class="card-title bg-white bg-opacity-25 rounded-pill ">${music[i].title}</h5>
-    //     <audio controls style="width: 180px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
-    //     <a href="${music[i].artist.link}" class="btn btn-primary">Go to link</a></div>`
-    // }
-    
+		console.log("funzione playCard", play);
+	}
 
-        
+	function pauseCard() {
+		audioElement.pause();
 
-    
- 
-    // const searchBar = document.getElementById('searchBar');
- 
-    // console.log(searchBar);
-    // searchBar.addEventListener('keyup',(e) =>{
-    //     const searchString = e.target.value;
-    //     const filteredAlbum = music.filter( song =>{
-    //     return song.title.toLowerCase().includes(searchString.toLowerCase())
-    // });
+		play = false;
+
+		console.log("funzione pauseCard", play);
+	}
+}
+
+// for (let i=0; i<music.length; i++){
+//     let showcard = document.getElementById('cards')
+//     showcard.innerHTML += `<div class="card bg-transparent border-0">
+//     <img src="${music[i].album.cover_medium}" class="card-img-top rounded-circle" alt="cover album Maneskin">
+//     <div class="card-body">
+//     <h5 class="card-title bg-white bg-opacity-25 rounded-pill ">${music[i].title}</h5>
+//     <audio controls style="width: 180px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
+//     <a href="${music[i].artist.link}" class="btn btn-primary">Go to link</a></div>`
+// }
+
+const searchBar = document.getElementById("searchBar");
+console.log(searchBar);
+const urlBase = "q=radiohead";
+function searchArtist() {
+	let params = new URLSearchParams(urlBase);
+	let query = params.get("q");
+	let input = searchBar.value;
+	console.log(query);
+	params.set("q", input);
+	let data = params.toString();
+	let newUrl = `https://striveschool-api.herokuapp.com/api/deezer/search?${data}`;
+	console.log(newUrl);
+	showApi(newUrl);
+}
+
+let arrayArtists = [
+	"Night Skinny",
+	"Radiohead",
+	"Lazza",
+	"Lizzo",
+	"Maneskin",
+	"Noyz Narcos",
+	"Salmo",
+	"Gemitaiz",
+	"Twenty one Pilots",
+	"Madonna",
+];
+
+for (let i = 0; i < arrayArtists.length; i++) {
+	let newUrl2 = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${arrayArtists[i]}`;
+	showApi(newUrl2);
+}
+
+// for (let i=0; i<music.length; i++){
+//     let showcard = document.getElementById('cards')
+//     showcard.innerHTML += `<div class="card bg-transparent border-0">
+//     <img src="${music[i].album.cover_medium}" class="card-img-top rounded-circle" alt="cover album Maneskin">
+//     <div class="card-body">
+//     <h5 class="card-title bg-white bg-opacity-25 rounded-pill ">${music[i].title}</h5>
+//     <audio controls style="width: 180px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
+//     <a href="${music[i].artist.link}" class="btn btn-primary">Go to link</a></div>`
+// }
+
+// const searchBar = document.getElementById('searchBar');
+
+// console.log(searchBar);
+// searchBar.addEventListener('keyup',(e) =>{
+//     const searchString = e.target.value;
+//     const filteredAlbum = music.filter( song =>{
+//     return song.title.toLowerCase().includes(searchString.toLowerCase())
+// });
 //     console.log(filteredSongs)
 //     let showcard = document.getElementById('cards')
 //     showcard.innerHTML=''
@@ -123,6 +138,4 @@
 
 // }
 
-
 // showApi()
-
