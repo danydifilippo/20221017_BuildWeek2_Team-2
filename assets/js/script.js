d26abafd7bbef306cd0ebb196ddff555e2ebf1aa
EarthDragon10 <lagudadev@gmail.com>
2022-10-18 19:33:17 +0200

    fix player spotify

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 async function showApi(n) {
-  let musiclink = await fetch(n);
-  let responseText = await musiclink.json();
-  let music = responseText.data;
-  console.log(music);
+	let musiclink = await fetch(n);
+	let responseText = await musiclink.json();
+	let music = responseText.data;
+	console.log(music);
 
-  function card() {
-    let showcard = document.getElementById("artists");
-    showcard.innerHTML += `<div  class="card border-0 resultCard">
+	function card() {
+		let showcard = document.getElementById("artists");
+		showcard.innerHTML += `<div  class="card border-0 resultCard">
             <img src="${music[0].artist.picture_medium}" class="card-img-top rounded-circle" alt="artista">
              <audio id='player-album'>
             <source src="${music[2].preview}" type='audio/mpeg'>
@@ -16,12 +16,12 @@ async function showApi(n) {
             <h5 class="card-title text-white">${music[0].artist.name}</h5>
             <h6>Artists</h6>
             </div>`;
-  }
-  card();
+	}
+	card();
 
-  function albums() {
-    let showAlbum = document.getElementById("albums");
-    showAlbum.innerHTML += `<div class="card border-0 bg-dark resultCard">
+	function albums() {
+		let showAlbum = document.getElementById("albums");
+		showAlbum.innerHTML += `<div class="card border-0 bg-dark resultCard">
         <div class="position-relative">
           <img src="${music[2].album.cover_medium}" class="card-img-top" alt="album">
           <audio id="player-album">
@@ -33,55 +33,66 @@ async function showApi(n) {
         <h5 class="card-title text-white">${music[2].album.title}</h5>
         <p>${music[2].artist.name}</p>
         </div>`;
-  }
-  albums();
+	}
+	albums();
 }
 
 let play = false;
 
 function togglePlay() {
-  let audioElement = document.getElementById("player-album");
+	let audioElement = document.getElementById("player-album");
 
-  console.log(play);
+	console.log(play);
 
-  play ? pauseCard() : playCard();
+	play ? pauseCard() : playCard();
 
-  function playCard() {
-    audioElement.play();
+	function playCard() {
+		audioElement.play();
 
-    play = true;
+		play = true;
 
-    console.log("funzione playCard", play);
-  }
-  function pauseCard() {
-    audioElement.pause();
+		console.log("funzione playCard", play);
+	}
+	function pauseCard() {
+		audioElement.pause();
 
-    play = false;
+		play = false;
 
-    console.log("funzione pauseCard", play);
-  }
+		console.log("funzione pauseCard", play);
+	}
 }
 
 const searchBar = document.getElementById("searchBar");
 console.log(searchBar);
 const urlBase = "q=radiohead";
 function searchArtist() {
-  let params = new URLSearchParams(urlBase);
-  let query = params.get("q");
-  let input = searchBar.value;
-  console.log(query);
-  params.set("q", input);
-  let data = params.toString();
-  let newUrl = `https://striveschool-api.herokuapp.com/api/deezer/search?${data}`;
-  console.log(newUrl);
-  showApi(newUrl);
+	let params = new URLSearchParams(urlBase);
+	let query = params.get("q");
+	let input = searchBar.value;
+	console.log(query);
+	params.set("q", input);
+	let data = params.toString();
+	let newUrl = `https://striveschool-api.herokuapp.com/api/deezer/search?${data}`;
+	console.log(newUrl);
+	showApi(newUrl);
 }
 
-let arrayArtists = ["Night Skinny", "Radiohead", "Lazza", "Lizzo", "Maneskin", "Noyz Narcos", "Salmo", "Gemitaiz", "Twenty one Pilots", "Madonna"];
+let arrayArtists = [
+	"Night Skinny",
+	"Radiohead",
+	"Lazza",
+	"Lizzo",
+	"Maneskin",
+	"Noyz Narcos",
+	"Salmo",
+	"Gemitaiz",
+	"Twenty one Pilots",
+	"Madonna",
+];
 
 for (let i = 0; i < arrayArtists.length; i++) {
-  let newUrl2 = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${arrayArtists[i]}`;
-  showApi(newUrl2);
+	let newUrl2 = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${arrayArtists[i]}`;
+	showApi(newUrl2);
 }
 
 // for (let i=0; i<music.length; i++){
@@ -119,3 +130,13 @@ for (let i = 0; i < arrayArtists.length; i++) {
 // }
 
 // showApi()
+
+// PLAYER
+
+function selectedHeart() {
+	let btnHeart = document.getElementById("heart");
+	let btnHeartFill = document.getElementById("heart-fill");
+
+	btnHeart.classList.toggle("d-none");
+	btnHeartFill.classList.toggle("d-none");
+}
