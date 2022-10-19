7b5dbd7ad6137e57af3de69b771b4019f381e39e
EarthDragon10 <lagudadev@gmail.com>
2022-10-19 19:47:34 +0200

    fix time audio player

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 async function showApi(n) {
-  let musiclink = await fetch(n);
-  let responseText = await musiclink.json();
-  let music = responseText.data;
-  console.log(music);
-  i = Number(Math.floor(Math.random() * 25));
+	let musiclink = await fetch(n);
+	let responseText = await musiclink.json();
+	let music = responseText.data;
+	console.log(music);
+	i = Number(Math.floor(Math.random() * 25));
 
-  function card() {
-    let showcard = document.getElementById("artists");
-    showcard.innerHTML += `<div  class="card border-0 resultCard">
+	function card() {
+		let showcard = document.getElementById("artists");
+		showcard.innerHTML += `<div  class="card border-0 resultCard">
             <div class="position-relative">
             <img src="${music[i].artist.picture_medium}" class="card-img-top rounded-circle" alt="artista" onclick="artistPage('${music[i].artist.name}')">
             <img class="w-25 position-absolute preview" src="./assets/img/play-button.png" alt="" onclick="playA('${music[i].preview}')">
@@ -16,12 +16,12 @@ async function showApi(n) {
             <h5 class="card-title text-white">${music[i].artist.name}</h5>
             <h6>Artists</h6>
             </div>`;
-  }
-  card();
+	}
+	card();
 
-  function albums() {
-    let showAlbum = document.getElementById("albums");
-    showAlbum.innerHTML += `<div class="card border-0 bg-dark resultCard">
+	function albums() {
+		let showAlbum = document.getElementById("albums");
+		showAlbum.innerHTML += `<div class="card border-0 bg-dark resultCard">
         <div class="position-relative">
         <img src="${music[i].album.cover_medium}" class="card-img-top" alt="album">  
             <img class="w-25 position-absolute preview" src="./assets/img/play-button.png" alt="" onclick="playA('${music[i].preview}')">
@@ -30,84 +30,85 @@ async function showApi(n) {
         <h5 class="card-title text-white">${music[i].album.title}</h5>
         <p>${music[i].artist.name}</p>
         </div>`;
-  }
-  albums();
+	}
+	albums();
 }
 
 async function returnApi(newA) {
-  let musicLink = await fetch(newA);
-  let respText = await musicLink.json();
-  let artista = respText.data;
-  console.log(artista);
-  let i = Number(Math.floor(Math.random() * 25));
-  let mn = Number(Math.floor(Math.random() * 4 + 1));
-  let sc = Number(Math.floor(Math.random() * 5));
-  let s = Number(Math.floor(Math.random() * 9));
-  let nr = Number(Math.floor(Math.random() * 700000 + 100000));
+	let musicLink = await fetch(newA);
+	let respText = await musicLink.json();
+	let artista = respText.data;
+	console.log(artista);
+	let i = Number(Math.floor(Math.random() * 25));
+	let mn = Number(Math.floor(Math.random() * 4 + 1));
+	let sc = Number(Math.floor(Math.random() * 5));
+	let s = Number(Math.floor(Math.random() * 9));
+	let nr = Number(Math.floor(Math.random() * 700000 + 100000));
 
-  function fillArtistPage() {
-    let showArtist = document.getElementById("headArtist");
-    showArtist.innerHTML = `<img src="${artista[i].artist.picture_xl}" alt="">
+	function fillArtistPage() {
+		let showArtist = document.getElementById("headArtist");
+		showArtist.innerHTML = `<img src="${artista[i].artist.picture_xl}" alt="">
     <p><img src="./assets/img/artist-verified-png.png" alt="" width="50px"><span>Verified artist</span></p>
     <h1>${artista[i].artist.name}</h1> 
     <p>18,375,540 monthly listener</p>`;
-    console.log(showArtist);
-    let follow = document.getElementById("follow");
-    follow.innerHTML += `<p><img class="w-25 position-absolute preview" src="" alt="" onclick="playA('${artista[i].preview}')">
+		console.log(showArtist);
+		let follow = document.getElementById("follow");
+		follow.innerHTML += `<p><img class="w-25 position-absolute preview" src="" alt="" onclick="playA('${artista[i].preview}')">
     <button>Follow</button>`;
-    let showSongs = document.getElementById("artistSongs");
-    console.log(showSongs);
-    for (let i = 1; i < 4; i++) {
-      showSongs.innerHTML += `<tr><th scope="row" onclick="playA('${artista[i].preview}')>${i}</th><td><img src="${artista[i].album.cover_small}" class="card-img-top" alt="album"></td>
+		let showSongs = document.getElementById("artistSongs");
+		console.log(showSongs);
+		for (let i = 1; i < 4; i++) {
+			showSongs.innerHTML += `<tr><th scope="row" onclick="playA('${artista[i].preview}')>${i}</th><td><img src="${artista[i].album.cover_small}" class="card-img-top" alt="album"></td>
     <td>${artista[i].title}</td>
     <td>${nr}</td>
     <td>${mn}:${sc}${s}</td>
     <td></td>
   </tr>`;
-    }
-  }
-  fillArtistPage();
+		}
+	}
+	fillArtistPage();
 }
 function artistPage(a) {
-  if (a === "Måneskin") {
-    a = "Maneskin";
-  }
-  let newUrl3 = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${a}`;
-  artista = returnApi(newUrl3);
-  console.log(artista);
-  fillArtistPage(artista);
+	if (a === "Måneskin") {
+		a = "Maneskin";
+	}
+	let newUrl3 = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${a}`;
+	artista = returnApi(newUrl3);
+	console.log(artista);
+	fillArtistPage(artista);
 }
 
 function playA(a) {
-  let aux = document.querySelector(".player");
-  if (aux.paused || aux.currentTime === 0 || aux.ended) {
-    aux.src = a;
-    aux.play();
-  } else {
-    aux.pause();
-  }
+	let aux = document.querySelector(".player");
+	if (aux.paused || aux.currentTime === 0 || aux.ended) {
+		aux.src = a;
+		aux.play();
+		setFillerBar();
+	} else {
+		aux.pause();
+	}
 }
 
 const searchBar = document.getElementById("searchBar");
 console.log(searchBar);
 const urlBase = "q=radiohead";
 function searchArtist() {
-  let params = new URLSearchParams(urlBase);
-  let query = params.get("q");
-  let input = searchBar.value;
-  console.log(query);
-  params.set("q", input);
-  let data = params.toString();
-  let newUrl = `https://striveschool-api.herokuapp.com/api/deezer/search?${data}`;
-  console.log(newUrl);
-  showApi(newUrl);
+	let params = new URLSearchParams(urlBase);
+	let query = params.get("q");
+	let input = searchBar.value;
+	console.log(query);
+	params.set("q", input);
+	let data = params.toString();
+	let newUrl = `https://striveschool-api.herokuapp.com/api/deezer/search?${data}`;
+	console.log(newUrl);
+	showApi(newUrl);
 }
 
 let arrayArtists = ["Radiohead", "Lazza", "Lizzo", "Maneskin"];
 
 for (let i = 0; i < arrayArtists.length; i++) {
-  let newUrl2 = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${arrayArtists[i]}`;
-  showApi(newUrl2);
+	let newUrl2 = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${arrayArtists[i]}`;
+	showApi(newUrl2);
 }
 
 // for (let i=0; i<music.length; i++){
@@ -149,59 +150,82 @@ for (let i = 0; i < arrayArtists.length; i++) {
 // PLAYER
 
 function selectedHeart() {
-  let btnHeart = document.getElementById("heart");
-  let btnHeartFill = document.getElementById("heart-fill");
-  let modalPlaceholder = document.getElementById("modal-placeholder");
-  let replaceTxtAdded = document.querySelector("#replace-txt-added");
-  let replaceTxtRemoved = document.querySelector("#replace-txt-removed");
+	let btnHeart = document.getElementById("heart");
+	let btnHeartFill = document.getElementById("heart-fill");
+	let modalPlaceholder = document.getElementById("modal-placeholder");
+	let replaceTxtAdded = document.querySelector("#replace-txt-added");
+	let replaceTxtRemoved = document.querySelector("#replace-txt-removed");
 
-  btnHeart.classList.toggle("d-none");
-  btnHeartFill.classList.toggle("d-none");
+	btnHeart.classList.toggle("d-none");
+	btnHeartFill.classList.toggle("d-none");
 
-  // Al primo click, mi deve apparire il placeholder con la scritta "aggiunto"
-  modalPlaceholder.classList.toggle("d-none");
-  // Dopo pochi secondi, mi deve sparí il placeholder
-  const showPlaceholdet = setTimeout(() => {
-    modalPlaceholder.classList.add("d-none");
-  }, 3000);
+	// Al primo click, mi deve apparire il placeholder con la scritta "aggiunto"
+	modalPlaceholder.classList.toggle("d-none");
+	// Dopo pochi secondi, mi deve sparí il placeholder
+	const showPlaceholdet = setTimeout(() => {
+		modalPlaceholder.classList.add("d-none");
+	}, 3000);
 
-  // quando clicco di nuovo sul cuoricino, deve comparire il placeholder
-  // con la scritta "rimosso"
-  if (replaceTxtRemoved.classList === "") {
-    modalPlaceholder.classList.remove("d-none");
+	console.log(replaceTxtRemoved.className);
 
-    replaceTxtAdded.classList.toggle("d-none");
-    replaceTxtRemoved.classList.toggle("d-none");
+	// quando clicco di nuovo sul cuoricino, deve comparire il placeholder
+	// con la scritta "rimosso"
 
-    const showRemoveTxt = setTimeout(() => {
-      modalPlaceholder.classList.add("d-none");
-    }, 3000);
-  }
-
-  // CHIEDERE A LIDIA PERCHE FUNZIONA QUESTA MIA LOGICA
-  replaceTxtAdded.classList.toggle("d-none");
-  replaceTxtRemoved.classList.toggle("d-none");
+	replaceTxtAdded.classList.toggle("d-none");
+	replaceTxtRemoved.classList.toggle("d-none");
 }
 
 function selectedPlayPause() {
-  let btnPlay = document.getElementById("btn_play");
-  let btnPause = document.getElementById("btn_pause");
+	let btnPlay = document.getElementById("btn_play");
+	let btnPause = document.getElementById("btn_pause");
 
-  btnPlay.classList.toggle("d-none");
-  btnPause.classList.toggle("d-none");
+	btnPlay.classList.toggle("d-none");
+	btnPause.classList.toggle("d-none");
 }
 
 function selectedBtnAudioColorizeGreen(event) {
-  console.log(event);
-  let btnSelected = event.querySelector(".bi");
+	console.log(event);
+	let btnSelected = event.querySelector(".bi");
 
-  console.log(btnSelected);
+	console.log(btnSelected);
 
-  btnSelected.classList.toggle("btn_colorize-green");
+	btnSelected.classList.toggle("btn_colorize-green");
 }
 
 function selectedModalControlDevic() {
-  let modalElement = document.getElementById("modal_control-device");
+	let modalElement = document.getElementById("modal_control-device");
 
-  modalElement.classList.toggle("d-none");
+	modalElement.classList.toggle("d-none");
+}
+
+// creare una funzione che, al click del preview-audio, parte il play, scorre il tempo
+// sulla sinistra, la barra si riempie in 30s, si ferma il loop,
+// l'hover deve far diventare la barra in verde ed aggiungere (se ci riesci), una pallina bianca
+
+let seconds = 1;
+let stopAudioPreview = false;
+
+function setFillerBar() {
+	const progressTimeElement = document.querySelector("#progress-time");
+	const fillerBarElement = document.querySelector("#filler_bar-time");
+
+	console.log(progressTimeElement, fillerBarElement);
+
+	fillerBarElement.classList.toggle("animation_filler-bar");
+
+	const changeSeconds = setInterval(() => {
+		if (seconds < 10) {
+			seconds = "0" + `${seconds}`;
+		}
+
+		progressTimeElement.innerHTML = `0:${seconds}`;
+		seconds++;
+
+		if (seconds === 31) {
+			clearInterval(changeSeconds);
+		}
+		// console.log(typeof progressTimeElement.innerHTML);
+	}, 1000);
+
+	// ora devi fare in modo che il titolo e l'artista cambi di testo al click
 }
